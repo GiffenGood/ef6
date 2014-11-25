71ee2d8aea33e0390681f4a2a966adf6e564db1d
MB <MBeckius@gmail.com>
2014-11-25 17:01:56 -0500

    more stuff

--- EF6DAL/MedModelContext.cs
+++ EF6DAL/MedModelContext.cs
@@ -1,6 +1,11 @@
-﻿namespace EF6DAL
+﻿using System.Data.Entity;
+using EF6;
+
+namespace EF6DAL
 {
-    public class MedModelContext
+    public class MedModelContext : DbContext
     {
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Address> Addresses { get; set; }
     }
 }
