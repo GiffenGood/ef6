33eff7416cc3ee1a1b4a17259f32b2ce5880e657
MB <MBeckius@gmail.com>
2014-11-25 22:16:14 -0500

    added test.cs

---
+++ EF6DAL/Hack/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using EF6;
+using EF6DAL.Migrations;
+
+namespace EF6DAL.Hack
+{
+    public class Tests
+    {
+        public void GetPatients()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MedModelContext,Configuration>());
+            
+            using (var ctx = new MedModelContext())
+            {
+                ctx.Patients.Where(p=>p.Addresses.Count == 0).ToList().ForEach(p=>Debug.WriteLine(p.FullName));
+                //var pat = new Patient {First = "Matt", Last = "Bixby"};
+                //ctx.Patients.Add(pat);
+                //ctx.SaveChanges();
+            }
+        }
+    }
+}
